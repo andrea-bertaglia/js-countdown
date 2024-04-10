43e00e1a79d4832f26592b6c32ad10e0ca370ee9
andrea-bertaglia <andreabertaglia@gmail.com>
2024-04-10 18:08:05 +0200

    feat: countdown function

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@ console.log("test link script.js");
 const startButton = document.getElementById("start-btn");
 startButton.addEventListener("click", startCountdown);
 
+// dichiaro una variabile per i secondi
+let seconds = 10;
+
+// funzione di call back che si attiva al click del bottone
+function startCountdown() {
+    if (seconds > 1) {
+        seconds--;
+        console.log(seconds);
+    } else {
+        console.log("buon anno!");
+        clearInterval(stopTimer);
+    }
+    
+}
+
+const stopTimer = setInterval(startCountdown, 1000);
+
